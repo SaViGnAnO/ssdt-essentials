352993573485f09229f17187b0d5f0bea5f447ad
Brian Savignano <brian.savignano@brightlysoftware.com>
2022-07-26 00:40:06 -0700

    initial commit

---
+++ SSDTEssentials/Commands/FixUnnamedConstraintsCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace SSDTEssentials
+{
+    [Command(PackageIds.FixUnnamedConstraintsCommand)]
+    internal sealed class FixUnnamedConstraintsCommand : BaseCommand<FixUnnamedConstraintsCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("SSDTEssentials", "Button clicked");
+        }
+    }
+}
